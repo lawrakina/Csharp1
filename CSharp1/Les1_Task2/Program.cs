33406493a886e5896f0ef07f6a84d5ffc81d26da
lawrik <lawr_fenix@mail.ru>
2019-12-02 13:37:32 +0300

    Добавлен Les1_task2

---
+++ CSharp1/Les1_Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les1_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 2 \nРасчет индекса массы тела");
+            Console.WriteLine("При вводе данных следуйте 2 правилам: \n *допускается не целое значение \n *разделитель дробной части запятая \",\"");
+            Console.WriteLine("Введите массу тела в киллограмах:");
+            float weight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите рост в метрах:");
+            float height = float.Parse(Console.ReadLine());
+
+            float result = weight / (height * height);
+            Console.WriteLine($"Индекс массы тела при росте {height} и весе {weight} равен: {result.ToString("#.##")}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp1/Les1_Task2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Les1_Task2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Les1_Task2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a7857d71-4b0a-4635-8689-5ac257a0d8ed")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
