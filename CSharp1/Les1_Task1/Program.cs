7d4d7f71dfc9bd5dd8c9ffa00f794954f183eae3
lawrik <lawr_fenix@mail.ru>
2019-12-02 13:35:35 +0300

    Добавлен les1_task1

---
+++ CSharp1/Les1_Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les1_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Задание 1 \nАнкета, введите свои данные");
+            Console.WriteLine("Имя?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Фамилия?");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Возраст?");
+            string age = Console.ReadLine();
+            Console.WriteLine("Вес?");
+            string weight = Console.ReadLine();
+
+            Console.WriteLine("Задание 1 пункт 1");
+            Console.WriteLine("Добрый день, " + firstName + " " + lastName + ", ваш возраст: " + age + ", ваш вес: " + weight);
+
+
+            Console.WriteLine("Задание 1 пункт 2");
+            Console.WriteLine("Добрый день, {0} {1}, ваш возраст: {2}, ваш вес: {3}", firstName, lastName, age, weight);
+
+            Console.WriteLine("Задание 1 пункт 3");
+            Console.WriteLine($"Добрый день, {firstName} {lastName}, ваш возраст: {age}, ваш вес: {weight}");
+
+            Console.ReadKey();
+        }
+    }
+}
