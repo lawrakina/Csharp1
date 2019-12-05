d549064bb4454a8ae91ac73bc477692a2b20d505
lawrik <lawr_fenix@mail.ru>
2019-12-05 22:59:07 +0300

    Занятие 3

---
+++ CSharp1/Les3_Task1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les3_Task1
+{
+    struct Complex
+    {
+        public double A, B;
+
+        public Complex(double A, double B)
+        {
+            this.A = A;
+            this.B = B;
+        }
+
+        public string Print()
+        {
+            return $"{A}+{B}i";
+        }
+
+        //сумма
+        public static Complex Sum(Complex N1, Complex N2)
+        {
+            return new Complex(N1.A + N2.A, N1.B + N2.B);
+        }              
+        //вычитание
+        public static Complex Difference(Complex N1, Complex N2)
+        {
+            return new Complex(N1.A - N2.A, N1.B - N2.B);
+        }
+        //произведение
+        public static Complex Multiplication(Complex N1, Complex N2)
+        {
+            return new Complex(
+                (N1.A * N2.A)-(N1.B * N2.B), 
+                (N1.B * N2.A)+(N1.A * N2.B)
+                );
+        }
+        //деление
+        public static Complex Division(Complex N1, Complex N2)
+        {
+            return new Complex(
+                (N1.A * N1.B + N2.A * N2.B) / (N1.B * N1.B + N2.B * N2.B),
+                (N2.A * N1.B - N1.A * N2.B) / (N1.B * N1.B + N2.B * N2.B)
+                );
+        }
+    }
+
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Студент Маращук Алексей \n Задание 1 \n операции над комплексными числами");
+            Console.WriteLine("x = a + bi");
+
+            Complex N1, N2;
+
+            Console.Write("a1 = "); N1.A = Double.Parse(Console.ReadLine());
+            Console.Write("b1 = "); N1.B = Double.Parse(Console.ReadLine());
+            Console.Write("a2 = "); N2.A = Double.Parse(Console.ReadLine());
+            Console.Write("b2 = "); N2.B = Double.Parse(Console.ReadLine());
+
+            var Nsum = Complex.Sum(N1, N2);
+            Console.WriteLine($"N1 + N2 = {Nsum.Print()}");
+
+            var NDifference = Complex.Difference(N1, N2);
+            Console.WriteLine($"N1 - N2 = {NDifference.Print()}");
+
+            var NMultiplication = Complex.Multiplication(N1, N2);
+            Console.WriteLine($"N1 * N2 = {NMultiplication.Print()}");
+
+            var NDivision = Complex.Division(N1, N2);
+            Console.WriteLine($"N1 / N2 = {NDivision.Print()}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp1/Les3_Task2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les3_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Студент Маращук Алексей \n Задание 2 \n подсчет суммы всех нечетных положительных чисел");
+
+            int sum = 0;
+            string result = "";
+            while (true)
+            {
+                string s;//начальная строка
+                int a; //валидное число
+                bool b;//флаг валидного числа
+
+                Console.WriteLine("введите число: ");
+
+                s = Console.ReadLine();
+                b = int.TryParse(s, out a);
+
+                if (b)//проверка пройдена число корректное
+                {
+                    if (a == 0)
+                    {
+                        Console.WriteLine("Получен 0, ввод чисел прерван");
+                        Console.WriteLine($"sum = {sum}, корректные слагаемые суммы: {result}");
+                        break;
+                    }
+                    else if (a % 2 == 0)
+                    {
+                        Console.WriteLine($"{a} - не подходит");
+
+                    }
+                    else if (a > 0)
+                    {
+
+                        Console.WriteLine($"{a} - нечетное положительное число");
+                        sum += a;
+                        result += $" {a}";
+
+                        Console.WriteLine($"sum = {sum}, корректные слагаемые суммы: {result}");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Строка не прошла проверку, продолжаем ввод");
+                }
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
